d959a8b7f2a92f0bb393704c496b1abfc5a76bf5
WaMel04 <99422112+wamel04@users.noreply.github.com>
2022-08-12 21:03:45 +0900

    HeadEquip 완성

--- src/main/java/com/wamel/headequip/HeadEquip.java
+++ src/main/java/com/wamel/headequip/HeadEquip.java
@@ -11,10 +11,12 @@ public final class HeadEquip extends JavaPlugin {
     public void onEnable() {
         instance = this;
         getServer().getPluginManager().registerEvents(new Event(), instance);
+        getLogger().info("§a플러그인 활성화 by WaMel_");
     }
 
     @Override
     public void onDisable() {
+        getLogger().info("§c플러그인 비활성화 by WaMel_");
     }
 
     public static HeadEquip getInstance() {
